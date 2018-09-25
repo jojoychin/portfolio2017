bc3087bd2c7c4f976d8008bb9f0466db3cd714b3
Joanna Chin <jchin@gallagherdesign.com>
2018-09-25 18:57:34 -0400

    local wintersmith and changed executables

---
+++ build/js/custom.js
@@ -0,0 +1,40 @@
+var click;
+var paused;
+
+$(document).ready(function() {
+	$(".button-collapse").sideNav({
+		closeOnClick: true,
+	});
+
+	$('.materialboxed').materialbox();
+	// click = false;
+	// paused = false;
+
+	$('.carousel.carousel-slider').carousel({full_width: true});
+		autoplay();
+});
+
+function autoplay() {
+	// if (!click){
+		$('.carousel').carousel('next');
+		setTimeout(autoplay, 6000);
+	// }
+}
+
+$('.button-down a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 600);
+    return false;
+});
+
+// ***** NOTES ***** //
+
+//VID for JOJO
+// <div class="video-container">
+// <iframe width="640" height="360" src="https://www.youtube.com/embed/HKrXLUqouJQ" frameborder="0" allowfullscreen></iframe>
+// </div>
+//video: //www.youtube.com/embed/FyZroOKK28E
+
+//INTRO TEXT
+//I work as an <b> interaction designer</b> for <a href="http://www.gallagherdesign.com/" target="_blank">Gallagher & Associates</a>, designing multi-platform, physical and digital experiences and products for museum and corporate spaces.
